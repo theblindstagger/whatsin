553ba7f11826b64ae2a8d9162a78a47267c32c10
Mark Donnelly <theblindstagger@gmail.com>
2020-04-11 12:22:34 +1000

    Added routing to lists component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListsComponent } from './lists/lists.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "/", component: ListsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
